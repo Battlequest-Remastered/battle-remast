aa6444876620169411a011338e2aeb54aa7f5a73
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-27 13:20:01 -0500

    continued mobile testing

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,5 +10,34 @@
 @yield('content')
 
 <script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript">
+	function jqUpdateSize(){
+	    // Get the dimensions of the viewport
+	    var width = $(window).width();
+	    var height = $(window).height();
+
+	    console.log(width);
+	    if(width <= 770 && width > 500) {
+	    	$(".commandline").addClass("mobile");
+	    	$(".commandline").removeClass("supersmall");
+	    	$(".commandline").removeClass("reallysupersmall");
+	    } else if(width <= 500 && width > 410) {
+	    	$(".commandline").removeClass("mobile");
+	    	$(".commandline").addClass("supersmall");
+	    	$(".commandline").removeClass("reallysupersmall");
+	    } else if(width <= 410) {
+	    	$(".commandline").addClass("reallysupersmall");
+	   		$(".commandline").removeClass("mobile");
+	    	$(".commandline").removeClass("supersmall");
+		} else {
+	    	$(".commandline").removeClass("mobile");
+	    	$(".commandline").removeClass("supersmall");
+	    	$(".commandline").removeClass("reallysupersmall");
+	    }
+	};
+	$(document).ready(jqUpdateSize);
+	    // When the page first loads
+	$(window).resize(jqUpdateSize); 
+</script>
 </body>
 </html>
