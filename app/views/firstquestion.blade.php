515468aa625742348c111cb1414ef7f4c34d2274
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-27 15:04:27 -0500

    Took out a curly brace that was cauing html tags to show up instead of render

--- app/views/firstquestion.blade.php
+++ app/views/firstquestion.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-8">
 		<div class="commandline">
-			<span class="question">{{{$question->question}}}</span>
+			<span class="question">{{$question->question}}</span>
 			@foreach($answers as $answer)
 			<br>
 				<a href="{{{action('HomeController@question', array($answer->leads_to_id, $answer->id))}}}">{{{$answer->answer}}}</a>
