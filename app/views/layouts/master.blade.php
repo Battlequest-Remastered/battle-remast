1f71d883da5f688e27ad89bc8c5e0968954221a8
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-27 11:47:04 -0500

    continued testing for mobile

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,19 +10,5 @@
 @yield('content')
 
 <script type="text/javascript" src="/js/jquery.js"></script>
-<script type="text/javascript">
-	function jqUpdateSize(){
-	    // Get the dimensions of the viewport
-	    var width = $(window).width();
-	    var height = $(window).height();
-
-	    if(width <= 480) {
-	    	// $(".commandline").addClass("mobile")
-	    }
-	};
-	$(document).ready(jqUpdateSize);
-	    // When the page first loads
-	$(window).resize(jqUpdateSize); 
-</script>
 </body>
 </html>
